b0a094e32c5494065b7434a936e612e0181a49d8
Luis Antonio Obis Aparicio <lobis@unizar.es>
2022-01-19 11:47:37 +0100

    Added simple unit tests

---
+++ test/test_mesh_from_file.py
@@ -0,0 +1,32 @@
+import unittest
+
+import os
+
+from stl.mesh import Mesh
+
+
+class TestMeshFromFile(unittest.TestCase):
+    files_dir = os.path.abspath("../files")
+
+    def test_files_dir_exists(self):
+        self.assertTrue(os.path.isdir(self.files_dir), f"files directory '{self.files_dir}' does not exist")
+
+    def test_mesh_from_file_cube(self):
+        stl_file = os.path.join(self.files_dir, "cube.stl")
+        self.assertTrue(os.path.isfile(stl_file))
+
+        mesh = Mesh.from_file(stl_file)
+
+        self.assertEqual(len(mesh.vectors), 12)  # cube has 12 triangular faces (2 per side)
+
+    def test_mesh_from_file_cylinder(self):
+        stl_file = os.path.join(self.files_dir, "cylinder.stl")
+        self.assertTrue(os.path.isfile(stl_file))
+
+        mesh = Mesh.from_file(stl_file)
+
+        self.assertEqual(len(mesh.vectors), 636)  # cylinder has 636 triangular faces for this mesh refinement
+
+
+if __name__ == '__main__':
+    unittest.main()
