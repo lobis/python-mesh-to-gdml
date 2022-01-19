dc154dc5951f2c2535707fb58fc5ceb3e5a27dfc
Luis Antonio Obis Aparicio <lobis@unizar.es>
2022-01-19 11:20:02 +0100

    Added optional schema validation

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from stl.mesh import Mesh
 import xml.etree.cElementTree as ET
 
 
+# import xmlschema
+
+
 def process_mesh(mesh, name):
+    print("processing mesh...")
     root = ET.Element("gdml",
                       **{"xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
                          "xsi:noNamespaceSchemaLocation": "https://service-spi.web.cern.ch/service-spi/app/releases/GDML/schema/gdml.xsd"}
                       )
 
     define = ET.SubElement(root, "define")
+    materials = ET.SubElement(root, "materials")
     solids = ET.SubElement(root, "solids")
     structure = ET.SubElement(root, "structure")
 
@@ -27,7 +32,7 @@ def process_mesh(mesh, name):
         return result
 
     vertices_to_name = dict()  # numpy array is not hashable, so we convert to string
-    for i, triangle in enumerate(mesh.vectors):
+    for triangle in mesh.vectors:
         face_vertices = {}
         for vertex in triangle:
             vertex_hash = vertex.data.tobytes()
@@ -42,7 +47,7 @@ def process_mesh(mesh, name):
     # world
     world_solid = ET.SubElement(solids, "box", name="world_solid", x="100", y="100", z="100")
     world_volume = ET.SubElement(structure, "volume", name="world")
-    physical_volume = ET.SubElement(world_volume, "physvol", name=f"{name}_volume")
+    physical_volume = ET.SubElement(world_volume, "physvol", name=name)
     ET.SubElement(physical_volume, "volumeref", ref=f"{name}_volume")
 
     ET.SubElement(world_volume, "materialref", ref="G4_AIR")
@@ -50,6 +55,8 @@ def process_mesh(mesh, name):
 
     setup = ET.SubElement(root, "setup", name="Default", version="1.0")
     ET.SubElement(setup, "world", ref="world")
+
+    print("processing mesh done!")
 
     return ET.ElementTree(root)
 
@@ -60,6 +67,9 @@ def main():
 
     tree = process_mesh(mesh, "mesh")
     ET.indent(tree)
+
+    # schema = xmlschema.XMLSchema("https://service-spi.web.cern.ch/service-spi/app/releases/GDML/schema/gdml.xsd")
+    # print(schema.is_valid(tree))
 
     tree.write("mesh.gdml", encoding="UTF-8", xml_declaration=True)
 
